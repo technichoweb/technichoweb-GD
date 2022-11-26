15bb2604cedfeaf052d572b93584969cc8bfa0fc
technichoweb <technichoweb@gmail.com>
2022-11-26 10:41:08 +0300

    update

--- wp-content/themes/technichoweb-smart/template-parts/section.php
+++ wp-content/themes/technichoweb-smart/template-parts/section.php
@@ -31,45 +31,35 @@
     <div id="home" class="slider-area">
         <header>
             <div class="owl-carousel owl-theme">
+                <?php
+                $args = array(
+                    'post_type' => 'gdslider',
+                    'posts_per_page' => -1
+                );
+
+                // Query the posts:
+                $query = new WP_Query($args);
+                // Loop through the obituaries:
+                while ($query->have_posts()) : $query->the_post();
+                $slideImage = get_field('gd_photo', $post->ID)['url'];
+                $slideSecondTitle = get_field('gd_second_title', $post->ID);
+                ?>
                 <div class="item">
-                    <img src="https://cdn.pixabay.com/photo/2017/10/24/10/30/business-2884023_960_720.jpg" alt="images not found">
+                    <img src="<?= $slideImage ?>" alt="images not found">
                     <div class="cover">
                         <div class="container">
                             <div class="header-content">
-                                <div class="line"></div>
-                                <h2>Teimagine Digital Experience with</h2>
-                                <h1>Start-ups and solutions</h1>
-                                <h4>We help entrepreneurs, start-ups and enterprises shape their ideas into products</h4>
+                                <?php if ($post->post_content !== ''): ?>
+                                    <div class="line"></div>
+                                <?php endif; ?>
+                                <h2><?= $slideSecondTitle ?></h2>
+                                <h1><?= the_title() ?></h1>
+                                <h4><?= $post->post_content; ?></h4>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="item">
-                    <img src="https://cdn.pixabay.com/photo/2016/03/09/09/22/workplace-1245776_960_720.jpg" alt="images not found">
-                    <div class="cover">
-                        <div class="container">
-                            <div class="header-content">
-                                <div class="line animated bounceInLeft"></div>
-                                <h2>Reimagine Digital Experience with</h2>
-                                <h1>Intelligent solutions</h1>
-                                <h4>We help entrepreneurs, start-ups and enterprises shape their ideas into products</h4>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="https://cdn.pixabay.com/photo/2017/05/04/16/37/meeting-2284501_960_720.jpg" alt="images not found">
-                    <div class="cover">
-                        <div class="container">
-                            <div class="header-content">
-                                <div class="line animated bounceInLeft"></div>
-                                <h2>Peimagine Digital Experience with</h2>
-                                <h1>Intelligent Solutions</h1>
-                                <h4>We help entrepreneurs, start-ups and enterprises shape their ideas into products</h4>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php endwhile; ?>
             </div>
         </header>
     </div>
