3090209e74a22dcc9cca25992d1670bbc0714445
technichoweb <technichoweb@gmail.com>
2022-11-28 15:03:48 +0300

    update

--- wp-content/themes/technichoweb-smart/template-parts/section.php
+++ wp-content/themes/technichoweb-smart/template-parts/section.php
@@ -91,140 +91,49 @@
             </div>
         </div> <!-- row -->
         <div class="row grid">
-            <div class="col-lg-4 col-sm-6 branding-3 planning-3">
-                <div class="single-portfolio mt-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="0.2s">
-                    <div class="portfolio-image">
-                        <img src="assets/images/portfolio-1.png" alt="">
-                        <div class="portfolio-overlay d-flex align-items-center justify-content-center">
-                            <div class="portfolio-content">
-                                <div class="portfolio-icon">
-                                    <a class="image-popup" href="assets/images/portfolio-1.png"><i
-                                                class="lni-zoom-in"></i></a>
-                                </div>
-                                <div class="portfolio-icon">
-                                    <a href="#"><i class="lni-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="portfolio-text">
-                        <h4 class="portfolio-title"><a href="#">Graphics Design</a></h4>
-                        <p class="text">Short description for the ones who look for something new. Awesome!</p>
-                    </div>
-                </div> <!-- single portfolio -->
-            </div>
-            <div class="col-lg-4 col-sm-6 marketing-3 research-3">
-                <div class="single-portfolio mt-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="0.4s">
-                    <div class="portfolio-image">
-                        <img src="assets/images/portfolio-2.png" alt="">
-                        <div class="portfolio-overlay d-flex align-items-center justify-content-center">
-                            <div class="portfolio-content">
-                                <div class="portfolio-icon">
-                                    <a class="image-popup" href="assets/images/portfolio-2.png"><i
-                                                class="lni-zoom-in"></i></a>
-                                </div>
-                                <div class="portfolio-icon">
-                                    <a href="#"><i class="lni-link"></i></a>
+            <?php
+            $args = array(
+                'post_type' => 'gdportfolio',
+                'posts_per_page' => -1
+            );
+
+            // Query the posts:
+            $query = new WP_Query($args);
+            // Loop through the obituaries:
+            while ($query->have_posts()) : $query->the_post();
+                $image = get_field('gd_photo', $post->ID);
+                $slug = '';
+                $defaultImage = get_template_directory_uri()."/assets/images/without_picture.png";
+                $postName = explode('-', $post->post_name);
+                if (isset($postName) && array_key_exists(0, $postName)) {
+                    $slug = $postName[0];
+                }
+                ?>
+                <div class="col-lg-4 col-sm-6 <?= $slug ?>">
+                    <div class="single-portfolio mt-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="0.2s">
+                        <div class="portfolio-image">
+                            <img src="<?= $image ?? $defaultImage ?>" alt="portfolio">
+                            <div class="portfolio-overlay d-flex align-items-center justify-content-center">
+                                <div class="portfolio-content">
+                                    <div class="portfolio-icon">
+                                        <a class="image-popup" href="<?= $image ?? $defaultImage ?>"><i class="lni-zoom-in"></i></a>
+                                    </div>
+                                    <div class="portfolio-icon">
+                                        <a href="#"><i class="lni-link"></i></a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="portfolio-text">
-                        <h4 class="portfolio-title"><a href="#">Graphics Design</a></h4>
-                        <p class="text">Short description for the ones who look for something new. Awesome!</p>
-                    </div>
-                </div> <!-- single portfolio -->
-            </div>
-            <div class="col-lg-4 col-sm-6 branding-3 marketing-3">
-                <div class="single-portfolio mt-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="0.7s">
-                    <div class="portfolio-image">
-                        <img src="assets/images/portfolio-3.png" alt="">
-                        <div class="portfolio-overlay d-flex align-items-center justify-content-center">
-                            <div class="portfolio-content">
-                                <div class="portfolio-icon">
-                                    <a class="image-popup" href="assets/images/portfolio-3.png"><i
-                                                class="lni-zoom-in"></i></a>
-                                </div>
-                                <div class="portfolio-icon">
-                                    <a href="#"><i class="lni-link"></i></a>
-                                </div>
-                            </div>
+                        <div class="portfolio-text">
+                            <h4 class="portfolio-title"><a href="#"><?php the_title() ?></a></h4>
+                            <p class="text"><?= $post->post_content ?></p>
                         </div>
-                    </div>
-                    <div class="portfolio-text">
-                        <h4 class="portfolio-title"><a href="#">Graphics Design</a></h4>
-                        <p class="text">Short description for the ones who look for something new. Awesome!</p>
-                    </div>
-                </div> <!-- single portfolio -->
-            </div>
-            <div class="col-lg-4 col-sm-6 planning-3 research-3">
-                <div class="single-portfolio mt-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="0.2s">
-                    <div class="portfolio-image">
-                        <img src="assets/images/portfolio-4.png" alt="">
-                        <div class="portfolio-overlay d-flex align-items-center justify-content-center">
-                            <div class="portfolio-content">
-                                <div class="portfolio-icon">
-                                    <a class="image-popup" href="assets/images/portfolio-4.png"><i
-                                                class="lni-zoom-in"></i></a>
-                                </div>
-                                <div class="portfolio-icon">
-                                    <a href="#"><i class="lni-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="portfolio-text">
-                        <h4 class="portfolio-title"><a href="#">Graphics Design</a></h4>
-                        <p class="text">Short description for the ones who look for something new. Awesome!</p>
-                    </div>
-                </div> <!-- single portfolio -->
-            </div>
-            <div class="col-lg-4 col-sm-6 marketing-3">
-                <div class="single-portfolio mt-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="0.4s">
-                    <div class="portfolio-image">
-                        <img src="assets/images/portfolio-5.png" alt="">
-                        <div class="portfolio-overlay d-flex align-items-center justify-content-center">
-                            <div class="portfolio-content">
-                                <div class="portfolio-icon">
-                                    <a class="image-popup" href="assets/images/portfolio-5.png"><i
-                                                class="lni-zoom-in"></i></a>
-                                </div>
-                                <div class="portfolio-icon">
-                                    <a href="#"><i class="lni-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="portfolio-text">
-                        <h4 class="portfolio-title"><a href="#">Graphics Design</a></h4>
-                        <p class="text">Short description for the ones who look for something new. Awesome!</p>
-                    </div>
-                </div> <!-- single portfolio -->
-            </div>
-            <div class="col-lg-4 col-sm-6 planning-3">
-                <div class="single-portfolio mt-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="0.7s">
-                    <div class="portfolio-image">
-                        <img src="assets/images/portfolio-6.png" alt="">
-                        <div class="portfolio-overlay d-flex align-items-center justify-content-center">
-                            <div class="portfolio-content">
-                                <div class="portfolio-icon">
-                                    <a class="image-popup" href="assets/images/portfolio-6.png"><i
-                                                class="lni-zoom-in"></i></a>
-                                </div>
-                                <div class="portfolio-icon">
-                                    <a href="#"><i class="lni-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="portfolio-text">
-                        <h4 class="portfolio-title"><a href="#">Graphics Design</a></h4>
-                        <p class="text">Short description for the ones who look for something new. Awesome!</p>
-                    </div>
-                </div> <!-- single portfolio -->
-            </div>
-        </div> <!-- row -->
-    </div> <!-- container -->
+                    </div> <!-- single portfolio -->
+                </div>
+            <?php endwhile; ?>
+        </div>
+    </div> <!-- row -->
+    </div> <!-- row -->
 </section>
 <!--====== portfolio PART ENDS ======-->
 
