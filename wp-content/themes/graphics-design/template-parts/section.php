d10688c6eadc4593bc21babc4077447856824c92
technichoweb <technichoweb@gmail.com>
2022-12-02 17:23:48 +0300

    update

--- wp-content/themes/graphics-design/template-parts/section.php
+++ wp-content/themes/graphics-design/template-parts/section.php
@@ -260,6 +260,41 @@
 </script>
 <!--====== portfolio PART ENDS ======-->
 
+<section id="contact" class="contact-area">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <div class="section-title text-center pb-20">
+                    <h3 class="title">Get in touch</h3>
+                    <p class="text">Stop wasting time and money designing and managing a website that doesn’t get
+                        results. Happiness guaranteed!</p>
+                </div> <!-- section title -->
+            </div>
+        </div> <!-- row -->
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="contact-two mt-50 wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.2s">
+                    <h4 class="contact-title">Lets talk about the project</h4>
+                    <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam unde repellendus
+                        delectus facilis quia consequatur maxime perferendis! Sequi, modi consequatur.</p>
+                    <ul class="contact-info">
+                        <li><i class="lni-money-location"></i> Elizabeth St, Melbourne, Australia</li>
+                        <li><i class="lni-phone-handset"></i> +333 789-321-654</li>
+                        <li><i class="lni-envelope"></i> hello@ayroui.com</li>
+                    </ul>
+                </div> <!-- contact two -->
+            </div>
+            <div class="col-lg-6">
+                <div class="contact-form form-style-one mt-35 wow fadeIn" data-wow-duration="1.5s"
+                     data-wow-delay="0.5s">
+                     <?php echo do_shortcode('[contact-form-7 id="29" title="contact"]') ?>
+                </div> <!-- contact form -->
+            </div>
+        </div> <!-- row -->
+    </div> <!-- container -->
+</section>
+
+
 <!-- Counter Section Start -->
 <section id="counter" class="section-padding">
     <div class="overlay"></div>
@@ -314,37 +349,3 @@
 </section>
 <!-- Counter Section End -->
 <!--====== CONTACT TWO PART START ======-->
-<section id="contact" class="contact-area">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-6">
-                <div class="section-title text-center pb-20">
-                    <h3 class="title">Get in touch</h3>
-                    <p class="text">Stop wasting time and money designing and managing a website that doesn’t get
-                        results. Happiness guaranteed!</p>
-                </div> <!-- section title -->
-            </div>
-        </div> <!-- row -->
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="contact-two mt-50 wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.2s">
-                    <h4 class="contact-title">Lets talk about the project</h4>
-                    <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam unde repellendus
-                        delectus facilis quia consequatur maxime perferendis! Sequi, modi consequatur.</p>
-                    <ul class="contact-info">
-                        <li><i class="lni-money-location"></i> Elizabeth St, Melbourne, Australia</li>
-                        <li><i class="lni-phone-handset"></i> +333 789-321-654</li>
-                        <li><i class="lni-envelope"></i> hello@ayroui.com</li>
-                    </ul>
-                </div> <!-- contact two -->
-            </div>
-            <div class="col-lg-6">
-                <div class="contact-form form-style-one mt-35 wow fadeIn" data-wow-duration="1.5s"
-                     data-wow-delay="0.5s">
-                     <?php echo do_shortcode('[contact-form-7 id="29" title="contact"]') ?>
-                </div> <!-- contact form -->
-            </div>
-        </div> <!-- row -->
-    </div> <!-- container -->
-</section>
-
