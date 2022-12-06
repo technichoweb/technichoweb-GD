a846b1745d31b873f90e61f9317d8853e58ef282
technichoweb <technichoweb@gmail.com>
2022-12-06 08:59:33 +0300

    update

--- wp-content/themes/graphics-design/assets/js/ajax.slick.js
+++ wp-content/themes/graphics-design/assets/js/ajax.slick.js
@@ -2,6 +2,8 @@ jQuery(document).ready(function () {
     $(".card_image").on("click", function () {
         let term_id = $(this).data('term');
         let ajaxurl = $(this).data('url');
+        let miseenpageElem = $('.miseenpages');
+        miseenpageElem.html("");
         $.ajax({
             url: ajaxurl,
             type: 'POST',
@@ -9,7 +11,6 @@ jQuery(document).ready(function () {
                 term_id: term_id,
                 action: 'get_images'
             }, success: function (response) {
-                let miseenpageElem = $('.miseenpages')
                 miseenpageElem.slick('destroy');
                 let output = ''
                 $.each(response, function(pos, el) {
