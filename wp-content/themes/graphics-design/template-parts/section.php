b562f131be361ccadfd65b92ff97bf6cf93e0899
technichoweb <technichoweb@gmail.com>
2022-12-14 11:10:12 +0300

    update

--- wp-content/themes/graphics-design/template-parts/section.php
+++ wp-content/themes/graphics-design/template-parts/section.php
@@ -171,7 +171,7 @@
             <?php endwhile; ?>
         </div>
     </div> <!-- row -->
-    <div class="modal fade" id="" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal fade" style="background-color: rgba(0, 0, 0, 0.8);" id="" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-body">
